22dcebcb2c4f9d2619c5e66b4b3b32cc58d3983d
Duball69 <120228301+duball69@users.noreply.github.com>
2024-05-16 13:08:14 +0100

    Raw CSS

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,14 @@
-  import React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css'; // Import the CSS file for styling
 
 function HomePage() {
- 
-
   return (
-    <div>
+    <div className="centered-content">
       <h1>Create your own token with 1 ETH of LP</h1>
-      <p>We help you create tokens with low costs and add 1 ETH to launch your memecoin!</p>
-    
-      <Link to="/factory">Let's Start</Link></div>
+      <p>We help you create tokens with low costs and add 1 ETH to launch your coin!</p>
+      <Link to="/factory" className="start-button">Let's Start</Link>
+    </div>
   );
 }
 
