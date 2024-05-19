7ae0a26c4fca4941097aa942ec63240319264ab4
Duball69 <120228301+duball69@users.noreply.github.com>
2024-05-19 21:27:23 +0100

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -19,9 +19,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
 
 const db = firebase.firestore();
 
