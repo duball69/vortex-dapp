f6cbde4e99db749297a8aa6802e89d0c6b283a53
Duball69 <120228301+duball69@users.noreply.github.com>
2024-05-19 05:32:03 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header({ connectWallet }) {
   return (
     <header>
          <div className="header-content">
+         <Link to="/">
       <img src="path/to/logo.png" alt="Logo" />
+      </Link>
       <button onClick={connectWallet}>Connect Wallet</button>
       </div></header>
   );
