c72b84b9f471e5f951e7592a3c760cdd8f4240be
Duball69 <120228301+duball69@users.noreply.github.com>
2024-05-21 18:48:04 +0100

    Menu Navigator Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
 function Header({ connectWallet }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+
+
   return (
     <header>
-         <div className="header-content">
-         <Link to="/">
-      <img src="logo512.png" alt="Logo" className="logo" />
-      </Link>
-      <button onClick={connectWallet}>Connect Wallet</button>
-      </div></header>
+      <div className="header-content">
+        
+                <Link to="/">
+                    <img src="logo512.png" alt="VortexLogo png" className="logo" />
+                </Link>
+
+                {/* Burger Icon */}
+                <button className="burger-menu" onClick={toggleMenu}>
+                    &#9776;
+                </button>
+
+                {/* Navigation Menu */}
+                <nav className={`menu ${isOpen ? "open" : ""}`}>
+                    <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+                    <Link to="/factory" onClick={() => setIsOpen(false)}>Launch</Link>
+                    <Link to="/staking" onClick={() => setIsOpen(false)}>Stake</Link>
+                    <a href="https://www.pornhub.com" target="_blank" rel="noopener noreferrer" onClick={() => setIsOpen(false)}>Docs</a>
+
+                    </nav>
+
+                <button onClick={connectWallet}>Connect</button>
+            </div></header>
   );
 }
 
