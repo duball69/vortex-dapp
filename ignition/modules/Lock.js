3cff05b855e6f61f383675f3876d61c6939f9766
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-11 00:28:00 +0100

    Update Lock.js

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -1,7 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
+const ONE_GWEI = 1_000_000_0000n;
 
 module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
