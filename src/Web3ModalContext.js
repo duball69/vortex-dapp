85678dad082d02521de5d0929b9edc0a76d1fea5
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-11 01:35:09 +0100

    A

--- src/Web3ModalContext.js
+++ src/Web3ModalContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { ethers } from "ethers";
 import { createWeb3Modal, useWeb3ModalAccount } from "@web3modal/ethers/react";
 
 const Web3ModalContext = createContext();
@@ -9,26 +10,6 @@ export const Web3ModalProvider = ({ children }) => {
     useWeb3ModalAccount(web3Modal);
 
   useEffect(() => {
-    // Define network configurations
-    const baseMainnet = {
-      chainId: 8453,
-      name: "Base",
-      currency: "ETH",
-      explorerUrl: "https://base.blockscout.com/",
-      rpcUrl: process.env.BASE_RPC_URL || "https://base.blockscout.com/", // Replace with your actual RPC URL
-    };
-
-    const sepoliaMainnet = {
-      chainId: 11155111,
-      name: "Sepolia",
-      currency: "ETH",
-      explorerUrl: "https://sepolia.etherscan.io/",
-      rpcUrl:
-        process.env.SEPOLIA_RPC_URL ||
-        "https://eth-sepolia.g.alchemy.com/v2/M87svOeOrOhMsnQWJXB8iQECjn8MJNW0",
-    };
-
-    // Web3Modal configuration
     const config = {
       metadata: {
         name: "Vortex Dapp",
@@ -36,26 +17,31 @@ export const Web3ModalProvider = ({ children }) => {
         url: "https://vortexdapp.com",
         icons: ["https://vortexdapp.com/favicon.ico"],
       },
-      enableEIP6963: true, // Enable MetaMask (EIP6963)
-      enableInjected: true, // Enable injected wallets (MetaMask, etc.)
-      enableCoinbase: false, // Enable Coinbase Wallet
-      defaultChainId: baseMainnet.chainId, // Set Base as the default chain
+      enableEIP6963: true, //metamask
+      enableInjected: true, //metamask
+      enableCoinbase: true, //coinbase
+      rpcUrl:
+        "https://eth-sepolia.g.alchemy.com/v2/M87svOeOrOhMsnQWJXB8iQECjn8MJNW0", // Ensure this matches your FactoryPage configuration
+      defaultChainId: 1,
     };
 
-    const projectId = process.env.REACT_APP_WALLETCONNECT_PROJECT_ID;
+    const sepoliaMainnet = {
+      chainId: 11155111,
+      name: "Sepolia",
+      currency: "ETH",
+      explorerUrl: "https://sepolia.etherscan.io/",
+    };
 
-    // Initialize Web3Modal
+    const projectId = process.env.WALLETCONNECT_PROJECT_ID;
+
     const initWeb3Modal = createWeb3Modal({
       config,
-      chains: [baseMainnet, sepoliaMainnet], // Include Base as the first chain
+      chains: [sepoliaMainnet],
       projectId,
       enableAnalytics: true,
     });
 
     setWeb3Modal(initWeb3Modal);
-
-    // Optional: Clear cached provider to avoid automatic reconnection to last used wallet
-    initWeb3Modal.clearCachedProvider();
   }, []);
 
   return (
