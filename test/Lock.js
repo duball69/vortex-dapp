966f2c82c59d6e995cffb9ed799a2d0a64524ef1
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-11 07:59:29 +0100

    Small bug fixes

--- test/Lock.js
+++ test/Lock.js
@@ -11,7 +11,7 @@ describe("Lock", function () {
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_0000;
+    const ONE_GWEI = 1_000_000_000;
 
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
