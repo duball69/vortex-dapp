29a4e65e6cff197fa8ca32de7aa23a8fff32afb5
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-11 03:08:28 +0100

    A

--- src/pages/TokenListPage.js
+++ src/pages/TokenListPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TokenListTable from "../components/TokenListTable.js";
+import TokenList from "../components/TokenList.js";
 import Header2 from "../components/Header2.js";
 import Footer from "../components/Footer.js";
 import "./TokenListPage.css"; // Import the CSS file for TokensPage
@@ -12,7 +13,7 @@ function TokensPage() {
         <TokenListTable />
       </div>
       <div className="token-list-mobile">
-        <TokenListTable />
+        <TokenList />
       </div>
       <Footer />
     </div>
