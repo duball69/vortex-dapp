4f57cd1c3b6ed9462d1e5c2b1fc7c8135c711955
Duball69 <120228301+duball69@users.noreply.github.com>
2024-08-11 08:31:08 +0100

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -7,7 +7,7 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConf ig = {
+const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "lp-provider-dapp.firebaseapp.com",
   projectId: "lp-provider-dapp",
